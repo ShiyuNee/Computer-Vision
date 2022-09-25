4cf2616c759084e54705d8e82d7382340c43479e
SDU-NSY <1026507192@qq.com>
2022-09-25 15:02:35 +0800

    Update E1.1.cpp

--- 实验一/源码/E1.1.cpp
+++ 实验一/源码/E1.1.cpp
@@ -10,22 +10,16 @@ int contrast_value;
 int bright_value;
 Mat source_image, transformed_image;
 
-static void ContrastAndBright(int pos , void* userdata)
-{
+static void ContrastAndBright(int pos , void* userdata){
     for (int y = 0; y < source_image.rows; y++)
-    {
         for (int x = 0; x < source_image.cols; x++)
-        {
-            for (int c = 0; c < 3; c++)
-            {
+            for (int c = 0; c < 3; c++){
                 //0<=contrast_value<=200
-                double t = ((source_image.at<Vec3b>(y, x)[c] - 127) / 255.00) * contrast_value * 0.1;
+                double t = ((source_image.at<Vec3b>(y, x)[c] - 127) / 255.00) * contrast_value * 0.1;//[-10, 10]
                 transformed_image.at<Vec3b>(y, x)[c] = saturate_cast<uchar>(source_image.at<Vec3b>(y, x)[c] * ((1.00 / (1.00 + exp(-t))) + 0.3) + bright_value - 100);
                 //int t = source_image.at<Vec3b>(y, x)[c];
                 //transformed_image.at<Vec3b>(y, x)[c] = saturate_cast<uchar>(t * ((contrast_value*0.1 / (1.00 + exp(-t))) + 1));
             }
-        }
-    }
     imshow("Display Window", source_image);
     imshow("Transformed Window", transformed_image);
 }
@@ -34,9 +28,8 @@ int main()
     source_image = imread("D:\\cLion\\project\\01.jpg");
 
     if (!source_image.data)
-    {
         cout << "图片读取失败！" << endl;
-    }
+
     transformed_image = Mat::zeros(source_image.size(), source_image.type());
 
     contrast_value = 20;
@@ -49,6 +42,5 @@ int main()
 
     waitKey(0);
     return 0;
-
 }
 
